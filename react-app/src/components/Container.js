92172cca6181a36e4f101f2727ce7444c29f9c53
Andrew Dotterer <adotterer@gmail.com>
2021-05-07 09:19:20 -0700

    site-wide style:

--- react-app/src/components/Container.js
+++ react-app/src/components/Container.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/container.css";
 
+// this is the style container for the entire site
 export default function Container(props) {
   return <div id="div__container">{props.children}</div>;
 }
