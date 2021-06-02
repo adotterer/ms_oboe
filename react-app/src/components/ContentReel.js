d7e3ba5c5eb54d04038639afa8c33db07650f9de
Andrew Dotterer <adotterer@gmail.com>
2021-06-02 14:01:50 -0700

    turned out didnt need grid

--- react-app/src/components/ContentReel.js
+++ react-app/src/components/ContentReel.js
@@ -5,11 +5,18 @@ import MilhaudAudio from "./audio/Milhaud2.wav";
 export function AudioPlayer(src) {
   return (
     <figure id="audio__grid">
-      <figcaption class="audio__title_composer">Milhaud</figcaption>
+      <figcaption class="audio__title_composer">
+        <span class="black__bg">La Création du monde</span>
+        <div>
+          <span class="black__bg composer">Milhaud 1892-1974)</span>
+        </div>
+      </figcaption>
       <div class="audio__player">
         <audio controls src={MilhaudAudio}></audio>
       </div>
-      <figcaption class="audio__performers">Matthew Shipp, oboe</figcaption>
+      <figcaption class="audio__performers">
+        <span class="black__bg">Matthew Shipp, oboe</span>
+      </figcaption>
     </figure>
   );
 }
