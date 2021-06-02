0b22ca1f6a8a4b4a4838999fff70ed0ec17c46e7
Andrew Dotterer <adotterer@gmail.com>
2021-06-02 11:20:19 -0700

    grid audio player

--- react-app/src/components/ContentReel.js
+++ react-app/src/components/ContentReel.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./styles/content_reel.css";
 import MilhaudAudio from "./audio/Milhaud2.wav";
 
 export function AudioPlayer(src) {
   return (
-    <figure>
-      <audio controls src={MilhaudAudio}></audio>
-      <figcaption>Milhaud</figcaption>
+    <figure id="audio__grid">
+      <figcaption class="audio__title_composer">Milhaud</figcaption>
+      <div class="audio__player">
+        <audio controls src={MilhaudAudio}></audio>
+      </div>
+      <figcaption class="audio__performers">Matthew Shipp, oboe</figcaption>
     </figure>
   );
 }
