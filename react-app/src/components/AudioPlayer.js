6d67df88989196ccf01c55b120440385f25e1f48
Andrew Dotterer <adotterer@gmail.com>
2021-11-02 14:11:04 -0700

    rework design of layout

--- react-app/src/components/AudioPlayer.js
+++ react-app/src/components/AudioPlayer.js
@@ -7,22 +7,31 @@ export function AudioPlayer({ src, musicInfo }) {
   return (
     <>
       <Upload />
-      <figure className="audio__grid">
-        <figcaption className="audio__title_composer">
-          <span className="black__bg">La Création du monde</span>
-          <div>
-            <span className="black__bg composer">
-              Darius Milhaud (1892-1974)
-            </span>
+      <div className="list__and__player">
+        <ul className="track__list">
+          <li>
+            <i>La Création du monde</i> - Darius Milhaud (1892-1974)
+            <figcaption>Matthew Shipp, oboe</figcaption>
+          </li>
+        </ul>
+
+        <figure className="audio__grid">
+          <figcaption className="audio__title_composer">
+            <span className="black__bg">La Création du monde</span>
+            <div>
+              <span className="black__bg composer">
+                Darius Milhaud (1892-1974)
+              </span>
+            </div>
+          </figcaption>
+          <div className="audio__player">
+            <audio controls src={MilhaudAudio}></audio>
           </div>
-        </figcaption>
-        <div className="audio__player">
-          <audio controls src={MilhaudAudio}></audio>
-        </div>
-        <figcaption className="audio__performers">
-          <span className="black__bg">Matthew Shipp, oboe</span>
-        </figcaption>
-      </figure>
+          <figcaption className="audio__performers">
+            <span className="black__bg">Matthew Shipp, oboe</span>
+          </figcaption>
+        </figure>
+      </div>
     </>
   );
 }
--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <div id="div__header">
       <h1>Matthew Shipp</h1>
+      <div className="oboe__img"></div>
     </div>
   );
 }
