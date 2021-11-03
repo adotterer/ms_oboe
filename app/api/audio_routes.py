63a61fcbc90293695719c1fcd56db9f73e7f7991
Andrew Dotterer <adotterer@gmail.com>
2021-11-02 20:21:16 -0700

    dataclasses import

--- app/api/audio_routes.py
+++ app/api/audio_routes.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, jsonify, session, request, redirect
+from app.models import Audio, db
+import json
 
 audio_routes = Blueprint('audio', __name__)
 
 
 @audio_routes.route('/all')
 def send_audio():
-    print("made it to the /audio/all route")
-    return {"audio": [{"msg": "audio files will go here"}]}
+    audio_query_results = Audio.query.all()
+    audio_files = jsonify(audio_query_results)
+    print(audio_files, "audio files")
+    return audio_files
--- app/models/audio.py
+++ app/models/audio.py
@@ -1,6 +1,17 @@
+from dataclasses import dataclass
 from .db import db
 
+# Python 3.7+ and Flask 1.1+ can use the built-in dataclasses package
+# https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json/57732785#57732785
+
+
+@dataclass
 class Audio(db.Model):
+    id: int
+    URL: str
+    title: str
+    composer: str
+    performers: str
     __tablename__ = 'audios'
 
     id = db.Column(db.Integer, primary_key=True)
