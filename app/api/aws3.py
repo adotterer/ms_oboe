6eeb5c7c587f62c2571832709d57ada55774b966
Andrew Dotterer <adotterer@gmail.com>
2021-11-06 02:09:07 -0700

    dead

--- app/api/aws3.py
+++ app/api/aws3.py
@@ -9,14 +9,26 @@ S3_BUCKET_NAME = Config.S3_BUCKET_NAME
 
 print("S3_BUCKET_NAME ".ljust(50, "."), S3_BUCKET_NAME)
 print("AWS_ACCESS_KEY_ID ".ljust(50, "."), AWS_ACCESS_KEY_ID)
-bucket_location = boto3.client('s3').get_bucket_location(Bucket=S3_BUCKET_NAME)
-# Bucket must be in params
+BUCKET_NAME = "mshippoboe"
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-)
+
+# Bucket must be in params
+try:
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+except Exception as e:
+    print("line22")
+    print("error=>>>", e
+          )
+try:
+    bucket_location = boto3.client(
+        's3').get_bucket_location(Bucket=BUCKET_NAME)
+except Exception as e:
+    print("line22")
+    print("error=>>>", e)
 
 
 def upload_file_to_s3(file, bucket_name, acl="public-read"):
@@ -37,7 +49,7 @@ def upload_file_to_s3(file, bucket_name, acl="public-read"):
         print("Something Happened: ", e)
         return e
 
-    return "https://{}.s3.{}.amazonaws.com/{}".format(S3_BUCKET_NAME, bucket_location['LocationConstraint'], file.filename)
+    return "https://{}.s3.{}.amazonaws.com/{}".format(BUCKET_NAME, bucket_location['LocationConstraint'], file.filename)
 
 
 def delete_file_on_s3(bucket_name, s3_key_to_delete):
