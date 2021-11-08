51ece6e57816babb554a81d0edd0e41a006984e4
Andrew Dotterer <adotterer@gmail.com>
2021-11-07 17:06:54 -0800

    upload css

--- react-app/src/components/Gallery.js
+++ react-app/src/components/Gallery.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import AuthContext from "./context/AuthContext";
 import "./styles/gallery.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 // function randomNumber(maxNumber) {
 //   return Math.ceil(Math.random() * maxNumber);
@@ -37,44 +38,51 @@ const imageData = [
 
 export default function Gallery() {
   const [zoomedPhotoId, setZoomedPhotoId] = useState(null);
+  const { authenticated } = useContext(AuthContext);
 
   return (
-    <div id="gallery__container">
-      <ImageList
-        style={{ margin: "0 auto" }}
-        sx={{ width: "80%", height: "100%" }}
-        variant="quilted"
-        cols={3}
-        rowHeight={250}
-      >
-        {imageData.map((image, i) => {
-          return (
-            <ImageListItem
-              style={{ cursor: "pointer" }}
-              onClick={() => setZoomedPhotoId(i)}
-              cols={zoomedPhotoId === i ? 2 : 1}
-              rows={zoomedPhotoId === i ? 2 : 1}
-              key={image.src + i}
-            >
-              <img
-                src={image.src}
-                // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={image.title}
-                loading="lazy"
-              />
-            </ImageListItem>
-          );
-        })}
+    <ImageList
+      style={{ margin: "0 auto" }}
+      id="gallery__container"
+      variant="quilted"
+      cols={3}
+      rowHeight={250}
+    >
+      {imageData.map((image, i) => {
+        return (
+          <ImageListItem
+            style={{ cursor: "pointer" }}
+            onClick={() => setZoomedPhotoId(i)}
+            cols={zoomedPhotoId === i ? 2 : 1}
+            rows={zoomedPhotoId === i ? 2 : 1}
+            key={image.src + i}
+          >
+            <img
+              src={image.src}
+              // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={image.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        );
+      })}
+      {authenticated && (
         <ImageListItem
-          style={{ cursor: "pointer" }}
+          id="upload__to_gallery"
           onClick={() => null}
           cols={1}
           rows={1}
           key={"uploader"}
         >
-          boom boom girl
+          <AddCircleIcon id="upload__icon__gallery" sx={{ fontSize: "8em" }} />
+          {/* <img
+              src={"AddCircleIcon"}
+              // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={"upload button"}
+              loading="lazy"
+            /> */}
         </ImageListItem>
-      </ImageList>
-    </div>
+      )}
+    </ImageList>
   );
 }
