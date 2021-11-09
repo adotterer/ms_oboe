26c1d34c4d5e7387c5351dacd9b76a4eda7f6f14
Andrew Dotterer <adotterer@gmail.com>
2021-11-09 11:09:17 -0800

    .

--- react-app/src/components/Gallery.js
+++ react-app/src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import AuthContext from "./context/AuthContext";
@@ -7,26 +7,19 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import ModalContext from "./context/ModalContext";
 import UploadImageModal from "./UploadImageModal";
 
-// function randomNumber(maxNumber) {
-//   return Math.ceil(Math.random() * maxNumber);
-// }
-
-const imageData = [
-  {
-    src: "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_section_1.jpg",
-  },
-  {
-    src: "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_section_2.jpg",
-  },
-  {
-    src: "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_3.jpg",
-  },
-];
-
 export default function Gallery() {
   const [zoomedPhotoId, setZoomedPhotoId] = useState(null);
   const { authenticated } = useContext(AuthContext);
   const [modalOpen, setModalOpen] = useState(false);
+  const [imageData, setImageData] = useState(null);
+
+  useEffect(() => {
+    if (!imageData) {
+      fetch("/api/images")
+        .then((res) => res.json())
+        .then((data) => setImageData(data));
+    }
+  }, [imageData]);
 
   return (
     <ModalContext.Provider value={{ modalOpen, setModalOpen }}>
@@ -37,24 +30,25 @@ export default function Gallery() {
         cols={3}
         rowHeight={250}
       >
-        {imageData.map((image, i) => {
-          return (
-            <ImageListItem
-              style={{ cursor: "pointer" }}
-              onClick={() => setZoomedPhotoId(i)}
-              cols={zoomedPhotoId === i ? 2 : 1}
-              rows={zoomedPhotoId === i ? 2 : 1}
-              key={image.src + "_" + i}
-            >
-              <img
-                src={image.src}
-                // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={image.title}
-                loading="lazy"
-              />
-            </ImageListItem>
-          );
-        })}
+        {imageData &&
+          imageData.map((image, i) => {
+            return (
+              <ImageListItem
+                style={{ cursor: "pointer" }}
+                onClick={() => setZoomedPhotoId(i)}
+                cols={zoomedPhotoId === i ? 2 : 1}
+                rows={zoomedPhotoId === i ? 2 : 1}
+                key={image.URL + "_" + i}
+              >
+                <img
+                  src={image.URL}
+                  // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={image.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            );
+          })}
         {authenticated && (
           <>
             <ImageListItem
