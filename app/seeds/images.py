b9b2dd43133cb37e21f0ca0fcc9606aa5ec63020
Andrew Dotterer <adotterer@gmail.com>
2021-11-09 10:45:03 -0800

    seed images

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -1,20 +1,30 @@
+from app.api.aws3 import delete_file_on_s3
 from app.models import db, Image
 
-imageData = [
+image_data = [
     {
         "URL": "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_section_1.jpg",
+        "title": "Philadelphia Oboe Section",
+        "description": ""
     },
     {
         "URL": "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_section_2.jpg",
+        "title": "",
+        "description": "",
     },
     {
         "URL": "https://mshippoboe.s3.us-west-1.amazonaws.com/phil_orch_3.jpg",
+        "title": "Verizon Hall",
+        "description": ""
     }]
 
 
 def seed_images():
 
-    db.session.add(demo)
+    for image in image_data:
+        new_image = Image(URL=image.URL, title=image.title,
+                          description=image.description)
+        db.session.add(new_image)
 
     db.session.commit()
 
