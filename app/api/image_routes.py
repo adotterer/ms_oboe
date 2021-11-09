f258b227f165dfee1266f58d493bf9ec9933140b
Andrew Dotterer <adotterer@gmail.com>
2021-11-09 11:02:40 -0800

    images/

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,18 +1,18 @@
 from flask_login import login_required
 from flask import Blueprint, jsonify, session, request, redirect
-from app.models import db
+from app.models import db, Image
 from .aws3 import delete_file_on_s3
 from flask import json
 
 image_routes = Blueprint('images', __name__)
 
 
-@image_routes.route('/all')
+@image_routes.route('/')
 def send_gallery():
     try:
-        # audio_query_results = Audio.query.all()
-        # audio_files = jsonify(audio_query_results)
-        return {"msg": "hi from Rosie"}
+        gallery = Image.query.all()
+        image_json = jsonify(gallery)
+        return image_json
     except Exception as e:
         print("error----->>".ljust(20, "."), e)
         return {"error": e.__dict__}
