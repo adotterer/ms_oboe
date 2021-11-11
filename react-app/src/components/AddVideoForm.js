4c75c2b8082a14eda572bf604bec95a101949a00
Andrew Dotterer <adotterer@gmail.com>
2021-11-10 20:07:49 -0800

    add new video styles

--- react-app/src/components/AddVideoForm.js
+++ react-app/src/components/AddVideoForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import ModalContext from "./context/ModalContext";
+import "./styles/video.css";
 
 export default function AddVideoForm() {
   const { setModalOpen } = useContext(ModalContext);
@@ -9,10 +10,18 @@ export default function AddVideoForm() {
   }
   return (
     <form onSubmit={handleSubmit} id="add__video__form">
+      <div>Add New Video:</div>
+     
       <label htmlFor="url">YouTube Embed URL:</label>
       <input name="url" type="text"></input>
-      <button>Submit</button>
-      <button onClick={() => setModalOpen(false)}>Cancel</button>
+      <label htmlFor="title">Title:</label>
+      <input name="title" type="text"></input>
+      <label htmlFor="description">Description:</label>
+      <input name="description" type="text"></input>
+      <span>
+        <button>Submit</button>
+        <button onClick={() => setModalOpen(false)}>Cancel</button>
+      </span>
     </form>
   );
 }
