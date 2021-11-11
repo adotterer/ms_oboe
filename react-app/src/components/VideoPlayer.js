09a782d5082f682bd29e6efe9b5279d370289f91
Andrew Dotterer <adotterer@gmail.com>
2021-11-10 19:32:06 -0800

    videos fetch call

--- react-app/src/components/VideoPlayer.js
+++ react-app/src/components/VideoPlayer.js
@@ -1,5 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import FeatureModal from "./FeatureModal";
+import AuthContext from "./context/AuthContext";
+import ModalContext from "./context/ModalContext";
 
 export default function VideoPlayer() {
-  return <div>Video Player</div>;
+  const { authenticated } = useContext(AuthContext);
+  const [videoData, setVideoData] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (!videoData) {
+      fetch("/api/videos/")
+        .then((res) => res.json())
+        .then((data) => setVideoData(data));
+    }
+    console.log(videoData, "<--- video data".padEnd(20, "%"));
+  }, [videoData]);
+  return (
+    <ModalContext.Provider value={{ modalOpen, setModalOpen }}>
+      <div>Hello from video</div>
+    </ModalContext.Provider>
+  );
 }
