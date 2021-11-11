e41951939a6cec50dce5a6a71d0ecd838e139df7
Andrew Dotterer <adotterer@gmail.com>
2021-11-10 20:48:11 -0800

    source

--- react-app/src/components/utils.js
+++ react-app/src/components/utils.js
@@ -1,3 +1,5 @@
+// https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api?rq=1
+
 export function getThumbnailURL(url) {
   const [_n, embedCode] = url.split("https://www.youtube.com/embed/");
   return `https://img.youtube.com/vi/${embedCode}/0.jpg`;
