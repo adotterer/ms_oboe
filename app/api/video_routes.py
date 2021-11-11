5f3455ee4ae42e842729f9c93c2a169237331b24
Andrew Dotterer <adotterer@gmail.com>
2021-11-10 19:19:44 -0800

    post route videos/

--- app/api/video_routes.py
+++ app/api/video_routes.py
@@ -16,3 +16,26 @@ def send_videos():
     except Exception as e:
         print("error---->>".ljust(20, "."), e)
         return {"error": e.__dict__}
+
+
+@video_routes.route("", methods=["POST"])
+@video_routes.route("/", methods=["POST"])
+@login_required
+def upload_video():
+    try:
+        url = request.form['URL']
+        title = request.form['title']
+        description = request.form['description']
+
+        new_video = Video(
+            URL=url,
+            title=title,
+            description=description
+        )
+
+        db.session.add(new_video)
+        db.session.commit()
+
+        return jsonify(new_video)
+    except Exception as e:
+        return {"error": e.__dict__}
