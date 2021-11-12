bd457b15dc9ac4ad9a08f924bf7e0f0c73ea133f
Andrew Dotterer <adotterer@gmail.com>
2021-11-11 20:40:54 -0800

    try catch, return null

--- react-app/src/components/utils.js
+++ react-app/src/components/utils.js
@@ -3,11 +3,16 @@
 export function getThumbnailURL(url) {
   console.log(url);
   console.log(getEmbedCode(url), "embed code, from url");
-  return `https://img.youtube.com/vi/${getEmbedCode(url)}/0.jpg`;
+  try {
+    return `https://img.youtube.com/vi/${getEmbedCode(url)}/0.jpg`;
+  } catch (e) {
+    console.log("error", e);
+    return "";
+  }
 }
 
 export function getEmbedCode(url) {
   const searchMatchIndex = url.search(/(?<==|\/)[a-zA-Z0-9]{11}/gm);
-
+  if (searchMatchIndex < 0) return null;
   return url.slice(searchMatchIndex);
 }
