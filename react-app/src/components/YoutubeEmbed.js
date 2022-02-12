2939c115ee192d4d4b7033217d82242d8b875f76
adotterer <adotterer@gmail.com>
2022-02-11 18:50:31 -0800

    .

--- react-app/src/components/YoutubeEmbed.js
+++ react-app/src/components/YoutubeEmbed.js
@@ -5,8 +5,6 @@ export default function YoutubeEmbed({ embedURL }) {
   return (
     <div className="video-responsive">
       <iframe
-        // width="853"
-        // height="480"
         src={embedURL}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
